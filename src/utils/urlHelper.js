74d739bb284c1b29bd8985110d711937d6b83f6b
tony.7@hotmail.fr <tony.7@hotmail.fr>
2021-08-21 17:18:35 +0200

    clean code

--- src/utils/urlHelper.js
+++ src/utils/urlHelper.js
@@ -44,19 +44,3 @@ function getParam(splitter, param) {
       : "");
   return new URLSearchParams(urlParams).get(param);
 }
-
-git filter-branch --env-filter '
-OLD_EMAIL="tony.7@hotmail.fr"
-CORRECT_NAME="tony.7@hotmail.fr"
-CORRECT_EMAIL="tony.7@hotmail.fr"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
